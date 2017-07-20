54daf7dc2d7ef497baca5ac9b93a1c76bba3912b
clement gilardy <gilardy.clement@gmail.com>
2017-07-20 22:16:27 +0200

    add composer.json and the database config

---
+++ app/database.php
@@ -0,0 +1,11 @@
+<?php
+
+public $default = array(
+    'host' => 'localhost',
+    'port' => '',
+    'login' => 'happy',
+    'password' => '',
+    'database' => 'bdd_happy',
+    'prefix' => 'happy',
+    'encoding' => 'utf8'
+);
