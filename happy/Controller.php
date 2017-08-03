7fc05fc7d1c70e63cd663c35de59fc4c9926f402
Hawklm <gilardy.clement@gmail.com>
2017-08-03 19:41:15 +0200

    add router + test

--- happy/Controller.php
+++ happy/Controller.php
@@ -2,9 +2,13 @@
 class Controller {
     
     private $twig;
+    public $params;
     
-    public function __construct($twig){
-        $this->twig = $twig;
+    public function __construct($params){
+        $templates = array(__DIR__.'/../app/Ressources/',__DIR__.'/../src/Home/views/');
+        $loader = new Twig_Loader_Filesystem($templates);
+        $this->twig =  new Twig_Environment($loader);
+        $this->params = $params;
     }
     
     protected function render($filename,array $params){
--- web/index.php
+++ web/index.php
@@ -3,20 +3,14 @@ require_once __DIR__.'/../app/Kernel.php';
 
 $kernel = new Kernel('prod');
 $kernel->load();
+ 
+$router = new Router();
+//Définition du dossier contenant les controlleur
+$router->setPath('../src/Home/controller/');
+// Si aucun controller n'est spécifié on appèlera HomeController et sa méthode index()
+$router->setDefaultControllerAction('HomeController','index');
+// En cas d'url invalid on appèlera le controller errorController et sa méthode alert()
+//$router->setErrorControllerAction('error', 'alert'); 
 
-$templates = array(__DIR__.'/../app/Ressources/',__DIR__.'/../src/Home/views/');
-$loader = new Twig_Loader_Filesystem($templates);
-$twig = new Twig_Environment($loader);
-
-$controller = new HomeController($twig);
-$action = 'index';
-
-if(method_exists($controller, $action)){
-    $controller->$action();
-} else {
-    echo 'erreur 404';
-}
-
-
-
-
+$router->addRule('/', array('controller' => 'HomeController', 'action' => 'index'));
+$router->load();
